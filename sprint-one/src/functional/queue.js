17141e786b0b2939efba22ca9a028ec5dae8d953
shabbyjoon <ShabnamMokhtarani@Shabnams-MacBook-Pro.local>
2018-08-01 15:39:12 -0700

    implements stack in the functional instaniation pattern

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,29 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var count = 0;
+  var key;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[key] = value;
+    count++;
+
   };
 
   someInstance.dequeue = function() {
+    count--;
+    delete storage[key-1]
+
   };
 
   someInstance.size = function() {
+    if( count < 0) {
+      return 0;
+    } else {
+      return count;
+    }
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,29 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var count = 0;
+  var key;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[count] = value;
+    count++;
   };
 
   someInstance.pop = function() {
+    count--;
+    var result = storage[count];
+    delete storage[count];
+    return result;
+    
   };
 
   someInstance.size = function() {
+    if( count < 0 ){
+      return 0;
+    } else {
+      return count++;
+    }
   };
 
   return someInstance;
