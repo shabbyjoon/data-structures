a7f15fd117688e9d04e1b389d71c67d3a24e5c2b
shabbyjoon <ShabnamMokhtarani@Shabnams-MacBook-Pro.local>
2018-08-01 23:00:03 -0700

    implements stack.js in pseudoclassical instantiation pattern

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,28 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.count = count;
+
 };
 
+var count = 0;
+var storage = {};
 
+
+Stack.prototype.push = function(value) {
+	storage[this.count] = value;
+    this.count++;
+};
+
+Stack.prototype.pop = function() {
+    this.count--;
+    var result = storage[this.count];
+    delete storage[this.count];
+    return result;
+};
+
+Stack.prototype.size = function() {
+	if ( this.count < 0 ){
+		return 0;
+	} else {
+	return this.count;
+    }
+};
