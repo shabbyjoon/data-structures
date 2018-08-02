52b00a393f526bb2383eb0c80a314dc0c3b07d0c
shabbyjoon <ShabnamMokhtarani@Shabnams-MacBook-Pro.local>
2018-08-01 23:01:52 -0700

    implements stack.js in es6 instantiation pattern

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,32 @@
 class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  constructor() {
+  constructor(count) {
+  	this.storage = {};
+  	this.count = 0;
+
+  }
+
+  push(value){
+   	this.storage[this.count] = value;
+    this.count++;
+
+  }
+
+  pop(){
+    this.count--;
+    var result = this.storage[this.count];
+    delete this.storage[this.count];
+    return result;
+
+  }
+
+  size(){
+  	if( this.count < 0){
+  		return 0;
+  	} else {
+  		return this.count;
+  	}
   }
 
 }
