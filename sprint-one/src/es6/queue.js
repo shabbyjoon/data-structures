f732e7c793811733753170feda81780fca9c549b
shabbyjoon <ShabnamMokhtarani@Shabnams-MacBook-Pro.local>
2018-08-01 23:02:40 -0700

    implements queue.js in es6 instantiation pattern

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,7 +1,35 @@
 class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  constructor() {
+  constructor(count) {
+  	this.storage = {};
+  	this.count = 0;
+  }
+
+  enqueue(value){
+  	this.storage[this.count] = value;
+    this.count++;
+
+  }
+
+  dequeue(){
+  	var result = this.storage[0];
+    delete this.storage[this.count];
+    this.count --;
+    for (var key in this.storage) {
+      this.storage[key - 1] = this.storage[key];
+    }
+    console.log(result)
+    return result;
+
+  }
+
+  size(){
+  	if( this.count < 0){
+		return 0;
+	} else {
+	return this.count;
+    }
   }
 
 }
