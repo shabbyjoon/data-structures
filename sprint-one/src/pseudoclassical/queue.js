37920b48dbc9dc5c93f2e722ba2e81844dff5da8
shabbyjoon <ShabnamMokhtarani@Shabnams-MacBook-Pro.local>
2018-08-01 23:00:50 -0700

    implements queue.js in pseudoclassical instantiation pattern

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,32 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.count = count;
 };
 
+var count = 0;
+var storage = {};
+
+Queue.prototype.enqueue = function(value) {
+  	 storage[this.count] = value;
+     this.count++;
+};
+
+Queue.prototype.dequeue = function() {
+    var result = storage[0];
+    delete storage[this.count];
+    this.count --;
+    for (var key in storage) {
+      storage[key - 1] = storage[key];
+    }
+    return result;
+
+};
+
+Queue.prototype.size = function() {
+	if( this.count < 0){
+		return 0;
+	} else {
+	return this.count;
+    }
+
+};
 
