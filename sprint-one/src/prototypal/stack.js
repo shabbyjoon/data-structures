ef01a17eb0a95d5ec1d7549d615e65384e5e0a52
shabbyjoon <ShabnamMokhtarani@Shabnams-MacBook-Pro.local>
2018-08-01 21:10:17 -0700

    implements stack.js in the prototypal pattern

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,33 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+
+  var someInstance = Object.create(stackMethods);
+  someInstance.count = count;
+  return someInstance;
+
 };
+var count = 0;
+var storage = {};
 
-var stackMethods = {};
+var stackMethods = {
+  push : function(value) {
+    storage[this.count] = value;
+    this.count++;
+	},
 
+  pop : function() {
+    this.count--;
+    var result = storage[this.count];
+    delete storage[this.count];
+    return result;
+  },
 
+  size : function() {
+  	if ( this.count < 0){
+  		return 0;
+  	} else {
+  		return this.count;
+  	}
+  	
+
+  }
+};
