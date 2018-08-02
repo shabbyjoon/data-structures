bb43c1314bf83234a9146dfe441dccbf2e0f025b
shabbyjoon <ShabnamMokhtarani@Shabnams-MacBook-Pro.local>
2018-08-01 21:12:14 -0700

    implements queue.js in the prototypal pattern

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,34 @@
+
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  
+  var someInstance = Object.create(queueMethods);
+  someInstance.count = count;
+  return someInstance;
+
 };
 
-var queueMethods = {};
+var count = 0;
+var storage = {};
+var queueMethods = {
+	enqueue : function(value) {
+	 storage[this.count] = value;
+     this.count++;
 
+	},
+	dequeue : function() {
+    var result = storage[0];
+    delete storage[this.count];
+    this.count --;
+    for (var key in storage) {
+      storage[key - 1] = storage[key];
+    }
+    return result;
 
+	},
+	size : function() {
+	if( this.count < 0 ){
+		return 0;
+	 }  
+	 return this.count;
+	}	
+};
