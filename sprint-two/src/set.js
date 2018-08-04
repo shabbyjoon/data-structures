a9fd636a1dec19348a528bc83a9f92bafccc2b34
shabbyjoon <ShabnamMokhtarani@Shabnams-MacBook-Pro.local>
2018-08-04 11:19:14 -0700

    implements set.js

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,19 +1,33 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  counter = 0;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage[this.counter] = item;
+  this.counter++;
 };
 
 setPrototype.contains = function(item) {
+  for (var prop in this._storage) {
+    if(this._storage[prop] === item) {
+      return true;
+    }
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
-};
+  for (var prop in this._storage) {
+    if(this._storage[prop] === item){
+      delete this._storage[prop];
+     }
+ }
+ };
 
 /*
  * Complexity: What is the time complexity of the above functions?
