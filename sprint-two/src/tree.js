3e692866281e75c982a33033312512e08e7c2c30
shabbyjoon <ShabnamMokhtarani@Shabnams-MacBook-Pro.local>
2018-08-03 19:59:52 -0700

    implements tree.js

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,21 +1,31 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
-
+  _.extend(newTree, treeMethods);
+  newTree.children = [];  
   return newTree;
 };
-
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
-treeMethods.contains = function(target) {
-};
-
+treeMethods.contains = function(target, node) {
+  if(target === node.value) {
+    return true;  
+    
+  } else if(node.children.length === 0) {
+    return; 
+  }else {
+    for(var i = 0; i < node.children.length; i++){
+      if(node.children[i].contains(target, node.children[i])){
+        return true;
+      } 
+    }
+  }
+  return false;
+ };
 
 
 /*
